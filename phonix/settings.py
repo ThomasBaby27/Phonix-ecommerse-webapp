2c279786d1de8d9e4f747dad09e66157208d79b4
thomas baby <thomasbaby273@gmail.com>
2025-06-06 04:03:12 +0530

    correctedd

--- phonix/settings.py
+++ phonix/settings.py
@@ -122,26 +122,26 @@ LOGOUT_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }   
+}
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'phonix_db',
-#         'USER': 'postgres',
-#         'PASSWORD': '8848662204',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }   
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'OPTIONS': {
+#             'timeout': 20,  # Seconds to wait for the lock to be released
+#         }
+#     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'timeout': 20,  # Seconds to wait for the lock to be released
-        }
-    }
-}
                     
 
 
