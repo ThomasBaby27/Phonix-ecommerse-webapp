2ebb74447f96575a26ab6c025c468bcd05cd7cb8
thomas baby <thomasbaby273@gmail.com>
2025-06-05 20:29:14 +0530

    change in settings.py 1

--- phonix/settings.py
+++ phonix/settings.py
@@ -122,16 +122,16 @@ LOGOUT_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'phonix_db',
-        'USER': 'postgres',
-        'PASSWORD': '8848662204',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'phonix_db',
+#         'USER': 'postgres',
+#         'PASSWORD': '8848662204',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 DATABASES = {
     'default': {
